1881bbc638c8fe1e2412a8ae13a7f5084c5b412f
이해준 <kyo2263@naver.com>
2024-08-09 19:09:01 +0900

    font color

--- src/App.js
+++ src/App.js
@@ -301,13 +301,13 @@ function MdItem(){
   return(
     <>
     {/* 100% */}
-      <div className='w-screen h-[1284px] bg-red-400'>
+      <div className='w-screen h-[1283px] bg-red-400'>
         {/* 인도네시아 나무 배경 */}
         <div className='w-full h-[355px] bg-cover' style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/indonesia.png)` }}>
         </div>
 
         {/* 갈색배경 */}
-        <div className='w-full h-[950px] bg-cover ' style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/indonesiaBG.png)` }}>
+        <div className='w-full h-[928px] bg-cover ' style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/indonesiaBG.png)` }}>
           
           <div>
             {/* 타이틀 */}
@@ -354,7 +354,7 @@ function StoreEDway(){
   return(
     <>
     {/* 100% */}
-      <div className='w-screen h-[500px] bg-white bg-cover ' style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/bottomImg.png)` }}>
+      <div className='w-screen h-[594px] bg-white bg-cover ' style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/bottomImg.png)` }}>
 
       </div>  
     </>
@@ -366,10 +366,10 @@ function Footer(){
   return (
     <>
     {/* 100% */}
-      <div className='w-screen bg-[#182855] h-[400px] flex  justify-center text-white text-xs'>
+      <div className='w-screen bg-[#182855] h-[460px] flex  justify-center text-[#afafaf] text-xs'>
         {/* 1200px bg-red-200 */}
         <div className='w-[1200px] h-full '>
-          <div className='flex justify-between mt-12'>
+          <div className='flex justify-between mt-[110px]'>
             <h4 className='font-semibold text-2xl 	'>EDIYA COFFEE</h4>
 
             <div>
@@ -381,10 +381,10 @@ function Footer(){
               </ul>
             </div>
           </div>
-          <div className='mt-16'>
-            <p className='mb-6'>개인정보처리 방침 | 이메일 무단 수집 거부</p>
+          <div className='mt-[80px]'>
+            <p className='mb-[31px]'>개인정보처리 방침 | 이메일 무단 수집 거부</p>
             <p>이디야커피(주) | 대표이사 강태성 | 서울특별시 강남구 논현로 636 이디야빌딩(서울특별시 강남구 논현동 221-17) | TEL:02-543-6467</p>
-            <p className='mb-6'>FAX:02-543-7191 | 사업자등록번호:107-86-16302 | 통신판매업 신고:강남 제 002519호</p>
+            <p className='mb-[31px]'>FAX:02-543-7191 | 사업자등록번호:107-86-16302 | 통신판매업 신고:강남 제 002519호</p>
             <p>Copyright 이디야커피(주) All Rights Reserved.</p>
           </div>
         </div>
