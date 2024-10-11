830a5417615e4e6ed16f078fe3f7607498f80ac2
이해준 <kyo2263@naver.com>
2024-10-11 17:18:21 +0900

    basename

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/EDIYACOFFEE">
+
     
       <App />
     
